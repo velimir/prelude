bdd39adc85d1762481a0134a51d4877baaa4976e
Grigory Starinkin <gstarinkin@alertlogic.com>
2013-12-26 18:20:05 +0400

    init.el converted to undecided-unix coding system
    added key bindings

--- init.el
+++ init.el
@@ -130,6 +130,12 @@
 (load-theme 'solarized-dark t)
 (set-frame-font "Ubuntu Mono-12:antialias=1")
 
+;; Key binding
+(global-set-key (kbd "C-?") 'help-command)
+(global-set-key (kbd "M-?") 'mark-paragraph)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+
 ;; Maximaze window on startup
 (when (eq system-type 'windows-nt)
   (defun w32-maximize-frame ()
