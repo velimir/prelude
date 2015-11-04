195fa753c3697d84c046df965ddb5f6a9756bca7
Grigory Starinkin <gstarinkin@alertlogic.com>
2015-11-04 13:25:14 +0000

    updated edts, fixed dynamic theme switches

--- init.el
+++ init.el
@@ -189,6 +189,12 @@
 (server-start)
 
 (load-theme 'solarized t)
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (let ((mode (if (display-graphic-p frame) 'light 'dark)))
+              (set-frame-parameter frame 'background-mode mode)
+              (set-terminal-parameter frame 'background-mode mode))
+            (enable-theme 'solarized)))
 ;;(set-frame-font "Ubuntu Mono-12:antialias=1")
 
 ;; Window number
