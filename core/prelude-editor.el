0604c04107cf1c8f116bd50777fd231f7aff675d
Grigory Starinkin <starinkin@gmail.com>
2018-10-02 08:20:08 +0100

    use vanilla kill ring

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -365,10 +365,6 @@ indent yanked text (with prefix arg don't indent)."
 ;; enable Prelude's keybindings
 (prelude-global-mode t)
 
-;; sensible undo
-(global-undo-tree-mode)
-(diminish 'undo-tree-mode)
-
 ;; enable winner-mode to manage window configurations
 (winner-mode +1)
 
