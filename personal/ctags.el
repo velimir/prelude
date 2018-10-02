9fa6973a380bb781dd7ee7d5c08794e25ffd4a0d
Grigory Starinkin <starinkin@gmail.com>
2018-10-02 08:22:42 +0100

    config ctags updates for erlang

---
+++ personal/ctags.el
@@ -0,0 +1,23 @@
+(setq ctags-update-prompt-create-tags nil)
+
+(defvar erlang-ctags-options
+  (list
+   "--fields=+iaSt"
+   "--extra=+q"
+   "--exclude='*.elc'"
+   "--exclude='*.class'"
+   "--exclude='.git'"
+   "--exclude='.svn'"
+   "--exclude='SCCS'"
+   "--exclude='RCS'"
+   "--exclude='CVS'"
+   "--exclude='EIFGEN'"
+   "--exclude=logs"
+   "--exclude=.eunit"
+   "--etags-include=/Users/gstarinkin/otp/20.1/TAGS"
+   "--links=no"))
+
+(defun my-erlang-ctags ()
+  (setq ctags-update-other-options erlang-ctags-options)
+  (turn-on-ctags-auto-update-mode))
+(add-hook 'erlang-mode-hook 'my-erlang-ctags)
