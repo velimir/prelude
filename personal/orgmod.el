a7d72bbf6d18a67259e06a302ec8c3d866c7fc80
Grigory Starinkin <starinkin@gmail.com>
2019-07-10 21:19:40 +0100

    customise latex export

--- personal/orgmod.el
+++ personal/orgmod.el
@@ -11,8 +11,20 @@
 (global-set-key (kbd "C-c C-x C-i") #'org-clock-in)
 (global-set-key (kbd "C-c C-x C-o") #'org-clock-out)
 
+;; HTML export
 (let* ((style-filename "org-custom-style.css")
        (style-path (expand-file-name style-filename prelude-personal-dir))
        (style-str (concat
                    "<link rel=\"stylesheet\" href=\"" style-path "\" />")))
   (setq org-html-head style-str))
+
+;; LaTeX export
+(setq org-latex-listings 'minted
+      org-latex-packages-alist '(("" "minted"))
+      org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+(setq org-latex-minted-options '(("breaklines" "true")
+                                 ("breakanywhere" "true")))
+
+(setq org-latex-toc-command "\\tableofcontents \\clearpage")
